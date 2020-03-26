cda01b26004886c821a96862e46d59960991e12b
Harish Krishnan <harishkrishnan1993@gmail.com>
2020-03-26 11:10:53 +0530

    Install and create next js app

---
+++ pages/contact.js
@@ -0,0 +1,7 @@
+const ContactPage = () => (
+  <div>
+    <h1>ContactPage</h1>
+  </div>
+);
+
+export default ContactPage;
---
+++ pages/index.js
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Link from "next/link";
+
+class Index extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  static async getInitialProps() {
+    const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    const { data } = res;
+    return {
+      posts: data
+    };
+  }
+
+  render() {
+    const { posts } = this.props;
+    return (
+      <div>
+        <h1>Index Page</h1>
+        <ul>
+          {posts.map(post => (
+            <li key={post.id}>
+              <Link href={`/post?id=${post.id}`}>
+                <a>{post.title}</a>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+// const Index = ({ posts }) => {
+//   return (
+//     <div>
+//       <h1>Index Page</h1>
+//       <ul>
+//         {posts.map(post => (
+//           <li key={post.id}>{post.title}</li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// };
+
+// Index.getInitialProps = async () => {
+//   const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
+//   const { data } = res;
+//   return {
+//     posts: data
+//   };
+// };
+export default Index;
